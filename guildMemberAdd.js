9a360af2b94a7d7bd5a6b026db80b719ae935ab7
aztek421 <38849929+aztek421@users.noreply.github.com>
2018-06-04 10:46:00 +0300

    Delete guildMemberAdd.js

--- guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.send('Sunucuya Hoş Geldin ! Ben de Seni Bekliyordum ^^ :tada: :wink: :innocent:');
-    member.guild.defaultChannel.send('Aramıza Hoşgeldin '+username+' , iyi vakit geçirmen dileğiyle ^^ :tada: :wink: :innocent:');
-}
