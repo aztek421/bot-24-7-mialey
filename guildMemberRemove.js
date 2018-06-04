1503f28829548bf911e3b093148b93499d493c8e
aztek421 <38849929+aztek421@users.noreply.github.com>
2018-06-04 10:46:39 +0300

    Delete guildMemberRemove.js

--- guildMemberRemove.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-  let guild = member.guild;
-  member.send('Sunucudan Ayrıldın ,bu beni üzdü :sob: :sob: :sob: ');
-  guild.defaultChannel.send('Aramızdan ayrıldı '+username+' ,:sob: :sob: :sob: ');
-};
